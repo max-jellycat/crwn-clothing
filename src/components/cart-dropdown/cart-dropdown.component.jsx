38e58981da7325625540790907d0e39bcc2645f0
max-jellycat <max@jellycat.fr>
2019-10-04 00:53:05 +0200

    updated app with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import CustomButton from '../custom-button/custom-button.component'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
 
 import './cart-dropdown.styles.scss'
 import CartItem from '../cart-item/cart-item.component'
@@ -17,8 +18,8 @@ const CartDropdown = ({ items }) => {
   )
 }
 
-const mapStateToProps = ({ cart: { items } }) => ({
-  items
+const mapStateToProps = state => ({
+  items: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown)
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemsCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemsCount}</span>
   </div>
 )
 
-const mapStateToProps = ({ cart: { items } }) => ({
-  items
+const mapStateToProps = state => ({
+  itemsCount: selectCartItemsCount(state)
 })
 
 const mapDispatchToProps = dispatch => ({
