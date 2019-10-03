b968d4dda9ed02e859ae6eff91a3206af35984ca
max-jellycat <max@jellycat.fr>
2019-10-03 23:38:24 +0200

    updated shop data with new data, updated cart utils to group items in cartReducer

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -154,7 +154,7 @@ const SHOP_DATA = [
   },
   {
     id: 4,
-    title: 'Women\'s',
+    title: 'Womens',
     routeName: 'womens',
     items: [
       {
@@ -203,7 +203,7 @@ const SHOP_DATA = [
   },
   {
     id: 5,
-    title: 'Men\'s',
+    title: 'Mens',
     routeName: 'mens',
     items: [
       {
@@ -244,6 +244,6 @@ const SHOP_DATA = [
       }
     ]
   }
-];
+]
 
-export default SHOP_DATA;
+export default SHOP_DATA
